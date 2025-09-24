d382fd2a4bfef24cc947de58e0c951bdae0a0a6e
hodlwarden <hodlwarden@gmail.com>
2025-09-24 12:17:51 -0400

    fix: deply issue

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,17 +4,6 @@
  */
 
 export * from './Button';
-export * from './Input';
 export * from './Card';
-export * from './Modal';
-export * from './Toast';
-export * from './Loading';
 export * from './ErrorBoundary';
 export * from './TokenLogo';
-export * from './WalletIcon';
-export * from './NetworkBadge';
-export * from './PriceImpactIndicator';
-export * from './SlippageSelector';
-export * from './AmountInput';
-export * from './TokenSelector';
-export * from './TransactionStatus';
